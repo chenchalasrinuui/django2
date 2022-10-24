7e443dd2c15a78d48b7100ad678554bb3c7be130
chenchalasrinuui <66296488+chenchalasrinuui@users.noreply.github.com>
2022-10-24 07:33:16 +0530

    latest

--- deployment/settings.py
+++ deployment/settings.py
@@ -41,7 +41,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',
     'myapp'
 ]
 
@@ -82,14 +81,21 @@ WSGI_APPLICATION = 'deployment.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'djongo',
-        'NAME': 'school',
-        'ENFORCE_SCHEMA': False,
-        'CLIENT': {
-                'host': 'mongodb+srv://stepup:stepup@cluster0.y2gqfip.mongodb.net/?retryWrites=true&w=majority'
-        }  
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'djongo',
+#         'NAME': 'school',
+#         'ENFORCE_SCHEMA': False,
+#         'CLIENT': {
+#                 'host': 'mongodb+srv://stepup:stepup@cluster0.y2gqfip.mongodb.net/?retryWrites=true&w=majority'
+#         }  
+#     }
+# }
 
 
 # Password validation
