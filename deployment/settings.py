0f64367224fb53df65f1f78590bf7fad2977fdbb
chenchalasrinuui <66296488+chenchalasrinuui@users.noreply.github.com>
2022-10-24 09:34:11 +0530

    latest

--- deployment/settings.py
+++ deployment/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-qg9b=omfyv^g+980_l(lg)%90u0%@%g%*4-oy6cb#y8_m$#0g6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com','.mongodb.net','.mongodb.com']
 
 
 # Application definition
@@ -80,24 +80,24 @@ WSGI_APPLICATION = 'deployment.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'djongo',
-#         'NAME': 'school',
-#        # 'ENFORCE_SCHEMA': False,
-#         'CLIENT': {
-#                 'host': 'mongodb+srv://stepup:stepup@cluster0.y2gqfip.mongodb.net/?retryWrites=true&w=majority',
-#                 'authMechanism': 'SCRAM-SHA-1'
-#         }  
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'djongo',
+        'NAME': 'school',
+        'ENFORCE_SCHEMA': False,
+        'CLIENT': {
+                'host': 'mongodb+srv://stepup:stepup@cluster0.y2gqfip.mongodb.net/?retryWrites=true&w=majority',
+                'authMechanism': 'SCRAM-SHA-1'
+        }  
+    }
+}
 
 
 # Password validation
